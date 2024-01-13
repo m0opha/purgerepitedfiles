4a95bee634e884d440259a1fae771a790361c1e5
m0opha <m0opha>
2024-01-13 12:40:56 -0300

    modified:   src/functions/__init__.py 	modified:   src/functions/help.py

--- src/functions/__init__.py
+++ src/functions/__init__.py
@@ -2,3 +2,4 @@ from .ArgHandler import ArgHandler
 from .CopyFilesTo import CopyFilesTo
 from .FindRepitedFiles import FindRepitedFiles
 from .GetHashingFiles import GetHashingFiles
+from .DeleteFiles import DeleteFiles
